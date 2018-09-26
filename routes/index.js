3341ec454695bd68878a16c0b3d3c7320b7cc24c
Ranchao Zhang <rc.z@me.com>
2018-09-26 15:21:17 -0400

    Done

--- routes/index.js
+++ routes/index.js
@@ -27,67 +27,86 @@ router.get('/', function(req, res, next) {
 	res.render('index', {});
 });
 
-/*
-{
-  "fieldname": "file",
-  "originalname": "resume.pdf",
-  "encoding": "7bit",
-  "mimetype": "application/pdf",
-  "destination": "uploads/",
-  "filename": "e1aded3c4dbab99bd9091e5125343572",
-  "path": "uploads/e1aded3c4dbab99bd9091e5125343572",
-  "size": 95914
-}
- */
-router.post('/print', upload.single('file'), (req, res, next) => {
+router.post('/print', (req, res, next) => {
+	console.log(req.body);
 	if (req.body.id === "") {
 		res.send("Please enter your Andrew ID.");
 		return;
-	} else if (req.file == null) { // undefined or null
-		res.send("Please send a file to print.");
-		return;
 	}
 
-	var cmd = FORMAT.format(req.body.id, req.file.originalname, req.file.path);
-	exec(cmd, (err, out, code) => {
-		if (!err && code == 0) {
-			msg = SUCCESS.format(req.body.id, req.file.originalname);
+	/*
+	 * 0: Andrew ID,
+	 * 1: Printer,
+	 * 2: Number of Copies,
+	 * 3: Page Range,
+	 * 4: Orientation,
+	 * 5: Side
+	 */
+	options = '-U {0} -d {1} -n {2}{3}{4} -o {5}'.format(
+		req.body.id,
+		req.body.printer,
+		req.body.num_copies,
+		req.body.range === '' ? '' : ' -o "{0}"'.format(req.body.range),
+		req.body.orientation == '' ? '' : ' -o {0}'.format(req.body.orientation),
+		req.body.sides
+	);
 
-			console.log(msg);
-			res.writeHead(200, {
-				'Content-Type': 'application/json'
-			});
-			res.write(JSON.stringify({
-				status: 'true',
-				id: req.body.id,
-				filename: req.file.originalname,
-				message: msg
-			}));
-			res.end();
-		} else {
-			msg = FAILURE.format(err);
-
-			console.log(msg);
-			res.writeHead(400, {
-				'Content-Type': 'application/json'
-			});
-			res.write(JSON.stringify({
-				status: 'false',
-				err: err,
-				message: msg
-			}));
-			res.end();
-		}
-
-		cmd = REMOVE.format(req.file.path);
-		exec(cmd, (err, out, code) => {
-			if (!err && code == 0) {
-				console.log('Deleted file "{0}"'.format(req.file.path));
-			} else {
-				console.log('Error deleting file "{0}"'.format(req.file.path));
-			}
-		});
-	});
+	res.render('print', {
+		address: 'cmu.print@aol.com',
+		title: 'New Print Query',
+		body: options
+	})
 });
+
+// router.post('/print', upload.single('file'), (req, res, next) => {
+// 	if (req.body.id === "") {
+// 		res.send("Please enter your Andrew ID.");
+// 		return;
+// 	} else if (req.file == null) { // undefined or null
+// 		res.send("Please send a file to print.");
+// 		return;
+// 	}
+
+// 	var cmd = FORMAT.format(req.body.id, req.file.originalname, req.file.path);
+// 	exec(cmd, (err, out, code) => {
+// 		if (!err && code == 0) {
+// 			msg = SUCCESS.format(req.body.id, req.file.originalname);
+
+// 			console.log(msg);
+// 			res.writeHead(200, {
+// 				'Content-Type': 'application/json'
+// 			});
+// 			res.write(JSON.stringify({
+// 				status: 'true',
+// 				id: req.body.id,
+// 				filename: req.file.originalname,
+// 				message: msg
+// 			}));
+// 			res.end();
+// 		} else {
+// 			msg = FAILURE.format(err);
+
+// 			console.log(msg);
+// 			res.writeHead(400, {
+// 				'Content-Type': 'application/json'
+// 			});
+// 			res.write(JSON.stringify({
+// 				status: 'false',
+// 				err: err,
+// 				message: msg
+// 			}));
+// 			res.end();
+// 		}
+
+// 		cmd = REMOVE.format(req.file.path);
+// 		exec(cmd, (err, out, code) => {
+// 			if (!err && code == 0) {
+// 				console.log('Deleted file "{0}"'.format(req.file.path));
+// 			} else {
+// 				console.log('Error deleting file "{0}"'.format(req.file.path));
+// 			}
+// 		});
+// 	});
+// });
 
 module.exports = router;
